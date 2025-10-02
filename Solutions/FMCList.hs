904c55f49540dbdcf2e03bb18b9ec46b7407c685
tainalmeidaa <tainaluise.a@gmail.com>
2025-10-02 10:30:23 -0300

    definindo product

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -78,7 +78,8 @@ sum [] = 0
 sum (x:xs) = x + sum xs
 
 product :: Num a => [a] -> a
-product = undefined
+product [] = 1
+product (x:xs) = x * product xs
 
 reverse :: [a] -> [a]
 reverse = undefined
